76521cad9e21701380356f50fa8796150f4f3611
Yan <y.passeniouk@gmail.com>
2022-04-24 14:52:38 +0200

    weekday fixed

--- app/inputParsers.hs
+++ app/inputParsers.hs
@@ -1,57 +1,88 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE BlockArguments      #-}
 module InputParsers where
-
+import System.IO.Unsafe
 import Events
-import Data.Void
+
 -- import Control.Applicative
 import  Text.Megaparsec
 import  Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
+import Data.Void
 import Data.Time
-
+import Data.Time.Calendar.MonthDay
+import Data.Tuple.Select
 import Data.Text ( Text, pack, intersperse )
 import Control.Monad.Trans.State (StateT)
 
 
 type MParser = Parsec Void String 
 
+date :: IO Day -- :: (year,month,day)
+date = getCurrentTime >>=  return . utctDay
+
+gregYear :: UTCTime -> Integer 
+gregYear x = sel1 (toGregorian $utctDay x)
+
+
 getDay :: MParser DayOfWeek
-getDay = do gday <- choice [
-             Monday <$ string' "mon" <* many alphaNumChar,
-             Tuesday <$ string' "tue"<* many alphaNumChar , 
-             Wednesday <$ string' "wed" <* many alphaNumChar, 
-             Thursday <$ string' "thu" <* many alphaNumChar, 
-             Friday <$ string' "fri"<* many alphaNumChar, 
-             Saturday <$ string' "sat" <* many alphaNumChar, 
-             Sunday <$ string' "sun" <* many alphaNumChar]
-            return gday
-
--- getrFreq :: MParser Vrfreq
--- getrFreq = do freq <- choice [ 
---                HOURLY  <$ hour, 
---                DAILY   <$ (try string' "daily" <|> string "every day"), 
---                WEEKLY  <$ (try string' "weekly" <|> string "every week"), 
---                MONTHLY <$ (try string' "monthly" <|> string "every month"), 
---                YEARLY  <$ (try string' "yearly" <|> string "every year")]
---               return freq
---             where hour = string' "hourly" <|> string' "every hour"
--- getISO :: MParser UTCTime
--- getISO = do 
--- 
+getDay = do choice 
+ [ Monday    <$ string' "mon" <* many alphaNumChar
+ , Tuesday   <$ string' "tue"<* many alphaNumChar 
+ , Wednesday <$ string' "wed" <* many alphaNumChar 
+ , Thursday  <$ string' "thu" <* many alphaNumChar 
+ , Friday    <$ string' "fri"<* many alphaNumChar 
+ , Saturday  <$ string' "sat" <* many alphaNumChar 
+ , Sunday    <$ string' "sun" <* many alphaNumChar ]
 
 
---how to extract value from the parser? 
+getrFreq :: MParser Vrfreq
+getrFreq = do choice 
+ [ HOURLY  <$ string' "hourly"
+ , HOURLY  <$ string' "every hour" 
+ , DAILY   <$ string' "daily"       -- <|> string "every day"), 
+ , WEEKLY  <$ string' "weekly"      -- <|> string "every week"), 
+ , MONTHLY <$ string' "monthly"     -- <|> string "every month"), 
+ , YEARLY  <$ string' "yearly"]       --  <|> string "every year")]
+
+getISO :: MParser Day
+getISO = do year <- L.decimal
+            try single '-' <|> space1
+            month <- L.decimal
+            try single '-' <|> space1
+            --satisfy (\x -> (x <= 12)) month
+            day <- L.decimal
+            return (fromGregorian year month day)  
+
+getDateMonth :: MParser Day
+getDateMonth = do month <- L.decimal
+                  space1
+                  day <- L.decimal
+                  return (fromGregorian ((gregYear $unsafePerformIO getCurrentTime) month day)) 
+
+getnDay :: MParser Day
+getnDay = do day <- string' "next "  *> getDay
+             return (nextWeekday day  $unsafePerformIO date)
+
+getnMonth :: MParser Day 
+getnMonth = do day <- string' "next" *> 
+               return (fromGregorian )
+
+
+ --how to extract value from the parser? A: parse will return the AST wrapped in Either 
 
 nextWeekday :: DayOfWeek -> Day -> Day -- -> UTCTime -> UTCTime 
 nextWeekday wd now = addDays x now
- where x = if diff < 0 then (-toInteger diff) else 7 - (toInteger diff)
-       diff =(fromEnum wd) - (fromEnum $dayOfWeek now)
+ where x = if diff < 0 then (-toInteger diff) else 7 -(toInteger diff)
+       diff = (fromEnum $dayOfWeek now) - (fromEnum wd)
 
---day of week on date . then toEnum on day .  (UTCTime)
+
 -- getMonth :: MParser 
 -- getMonth = undefined
 
