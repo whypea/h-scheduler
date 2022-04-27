980ba1a23514f9fa1ddad7f6f37cccf3139bd2c7
Yan <y.passeniouk@gmail.com>
2022-04-27 08:55:52 +0200

    Show instances

--- app/inputParsers.hs
+++ app/inputParsers.hs
@@ -20,17 +20,33 @@ import Data.Time.Calendar.MonthDay
 import Data.Tuple.Select
 import Data.Text ( Text, pack, intersperse )
 import Control.Monad.Trans.State (StateT)
-
+import Control.Monad (void)
 
 type MParser = Parsec Void String 
+
+---- !Helpers
+emptySingle :: (MonadParsec e s m, Token s ~ Char) => Token s -> m () --from source of space 
+emptySingle x = void $ single x
 
 date :: IO Day -- :: (year,month,day)
 date = getCurrentTime >>=  return . utctDay
 
+unsafeCurrentTime :: UTCTime
+unsafeCurrentTime = unsafePerformIO getCurrentTime 
+
 gregYear :: UTCTime -> Integer 
 gregYear x = sel1 (toGregorian $utctDay x)
 
+gregMonth :: UTCTime -> Int 
+gregMonth x = sel2 (toGregorian $utctDay x)
 
+gregDay :: UTCTime -> Int 
+gregDay x = sel3 (toGregorian $utctDay x)
+
+-- makeUTCTime :: Day -> DiffTime -> UTCTime
+-- makeUTCTime day time = 
+
+---- !Parsers
 getDay :: MParser DayOfWeek
 getDay = do choice 
  [ Monday    <$ string' "mon" <* many alphaNumChar
@@ -41,41 +57,63 @@ getDay = do choice
  , Saturday  <$ string' "sat" <* many alphaNumChar 
  , Sunday    <$ string' "sun" <* many alphaNumChar ]
 
+getMonth :: MParser Int
+getMonth = do choice 
+ [ 1    <$ string' "Jan" <* many alphaNumChar
+ , 2   <$ string' "Feb"<* many alphaNumChar 
+ , 3      <$ string' "Mar" <* many alphaNumChar 
+ , 4      <$ string' "Apr" <* many alphaNumChar 
+ , 5        <$ string' "May"<* many alphaNumChar 
+ , 6       <$ string' "Jun" <* many alphaNumChar 
+ , 7          <$ string' "Jul" <* many alphaNumChar 
+ , 8     <$ string' "Aug" <* many alphaNumChar 
+ , 9  <$ string' "Sep" <* many alphaNumChar 
+ , 10    <$ string' "Oct" <* many alphaNumChar 
+ , 11   <$ string' "Nov" <* many alphaNumChar 
+ , 12   <$ string' "Dec" <* many alphaNumChar ]
 
 getrFreq :: MParser Vrfreq
 getrFreq = do choice 
  [ HOURLY  <$ string' "hourly"
  , HOURLY  <$ string' "every hour" 
  , DAILY   <$ string' "daily"       -- <|> string "every day"), 
+ , DAILY   <$ string' "every day"       -- <|> string "every day"), 
  , WEEKLY  <$ string' "weekly"      -- <|> string "every week"), 
+ , WEEKLY  <$ string' "every week"      -- <|> string "every week"), 
  , MONTHLY <$ string' "monthly"     -- <|> string "every month"), 
- , YEARLY  <$ string' "yearly"]       --  <|> string "every year")]
+ , MONTHLY <$ string' "every month"     -- <|> string "every month"), 
+ , YEARLY  <$ string' "yearly"       --  <|> string "every year")]
+ , YEARLY  <$ string' "every year"]       --  <|> string "every year")]
 
 getISO :: MParser Day
 getISO = do year <- L.decimal
-            try single '-' <|> space1
+            try $emptySingle '-' <|> space1
             month <- L.decimal
-            try single '-' <|> space1
-            --satisfy (\x -> (x <= 12)) month
+            try $emptySingle '-' <|> space1
             day <- L.decimal
-            return (fromGregorian year month day)  
+            return (fromGregorian year month day) --Will clip the date  
+
+get
 
 getDateMonth :: MParser Day
 getDateMonth = do month <- L.decimal
-                  space1
+                  try $emptySingle '-' <|> space1
                   day <- L.decimal
-                  return (fromGregorian ((gregYear $unsafePerformIO getCurrentTime) month day)) 
+                  return (fromGregorian (gregYear unsafeCurrentTime) month day) 
 
 getnDay :: MParser Day
 getnDay = do day <- string' "next "  *> getDay
              return (nextWeekday day  $unsafePerformIO date)
 
 getnMonth :: MParser Day 
-getnMonth = do day <- string' "next" *> 
-               return (fromGregorian )
+getnMonth = do month <- string' "next " *> getMonth  
+               return (fromGregorian (gregYear unsafeCurrentTime) month (gregDay unsafeCurrentTime))
 
 
- --how to extract value from the parser? A: parse will return the AST wrapped in Either 
+getInterval :: MParser Interval --just a number
+getInterval = do a <- L.decimal 
+                  return a
+
 
 nextWeekday :: DayOfWeek -> Day -> Day -- -> UTCTime -> UTCTime 
 nextWeekday wd now = addDays x now
@@ -85,6 +123,3 @@ nextWeekday wd now = addDays x now
 
 -- getMonth :: MParser 
 -- getMonth = undefined
-
---nextMonth :: Day -> Day 
---next
--- newnotes.md
+++ newnotes.md
@@ -1,31 +1,47 @@
-"employee" a different category of task?
-    Routines vs 
-"preference" as a way to encode deadlines?
-    sooner/more urgent
-
-
-"machines" as locations/categories, with extra time given for travel?
 
 input
     either a new user-generated ics
-    or a file to be modifie
+    or a file to be modified
     options
         asking for more options if they aren't filled out?
         defaults for options?
         be verbose?
 
 input is parsed (newFile ) into Data.Time (Day/time), Timezone
-    what do I do with a parser/megaparsec/optparse?
-Day/time will be distributed by the solver, 
-    SOLVER IS THE MOST DIFFICULT PART OF THIS
+    what do I do with a parser/megaparsec/optparse? X
+        megaparsec works better
+Day/time will be distributed by the solver
+Each Type will be evaluated by a function in order
+
+State as a way of checking? 
+
+Install containers
+Types cli -> Vevent -> solver (potentially no date) -> Vevent, rrule -> 
+How to get between cli and megaparsec
+    I guess just call a function?
+Solver: 
+Add to the list of values,
+Check the same dates/times
+Check for recurrence 
+   (So you need recurrance rule and a parser for it)
+   how would you encode recurrence ?
+    The ByDay/ByMonth, then check if that mod whatever is on. 
+    Count: check in the obvious way first? 
+ Due windows
+Add to list
+Continue
+
+Error messages in megaparsec
+    For each of the different parsers, how would that be done?
+    
 
 
 
 icalendar file printing options
     - multiple events in one call
     - repeating rule
-    - 
-    - 
+    - UID generator
+    -
 -- Define bounds for time scheduling (sleep)
     -- As defaults/present at startup
 -- Preference/"balance" constraints
@@ -37,21 +53,11 @@ icalendar file printing options
     -Ordered list -> Deterministic state shop
     -Prioritized list -> Job shop
     -Unordred list -> Project
-
--- https://datatracker.ietf.org/doc/html/rfc5545#section-3.3
---Make a parser for this
---Content lines are delimited by a line break, CRLF sequence
---File must include (in contentlines) 
---recurrance rrule
---"contentline   = name *(";" param ) ":" value CRLF" 
-    -- name = iana-token / x-name
-    -- iana-token    = 1*(ALPHA / DIGIT / "-")
-
-cli should print an entire calendar/change one
+( ) cli print an entire calendar/change one
 
 
 --shorthands (with try)
-    -- Mon(day)    -> closest monday x
+    -- Mon(day)    -> closest monday X
     -- 0103        -> closest 1st of march
     --"in a year"  -> from today
     -- 3am (mon)   -> closest (monday)
@@ -68,9 +74,22 @@ cli should print an entire calendar/change one
 ----recurrance
 ----type of events
 
+
+
 --utkast
 --leap = (\x -> (x `mod` 4 == 0) /= (x `mod` 100 /= 0 && x `mod` 400 == 0))
 --satisfy (\x -> x > 0 && x < (monthLength (isLeapYear year) month)) day
 --satisfy (\x -> (x > 2000 && x <= 2022) || (x <= 22)) year 
 --satisfy (\x -> (x <= 12)) month
+--data Event a = Calendar a | Event a| Todo a
+--data Datetime = Dt Integer Integer Integer deriving (Show, Eq, Ord)
 
+
+-- https://datatracker.ietf.org/doc/html/rfc5545#section-3.3
+--Make a parser for this
+--Content lines are delimited by a line break, CRLF sequence
+--File must include (in contentlines) 
+--recurrance rrule
+--"contentline   = name *(";" param ) ":" value CRLF" 
+    -- name = iana-token / x-name
+    -- iana-token    = 1*(ALPHA / DIGIT / "-")
