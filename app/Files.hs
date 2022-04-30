a6f845ee0fa0eed351b525aab89cbd7af5eb5887
Yan <y.passeniouk@gmail.com>
2022-04-30 20:57:47 +0200

    printTest, refactoring,

--- app/Files.hs
+++ app/Files.hs
@@ -7,13 +7,34 @@ module Files where
 
 import InputParsers
 import Events
-import Solver
+
 
 import System.IO
+import System.Directory
+
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import Data.Time.LocalTime
+
+-- getICS :: FilePath -> Maybe Handle 
+-- getICS (fp) = fp 
+
+testCal = Vcalendar "123" (Version "2") GREGORIAN (TZ (TimeZone 0 False "UTC")) []
+
+makeICS :: String -> IO Handle
+makeICS name = openFile (name ++ ".ics") WriteMode 
+                 
+
+writeVCalendar :: Handle -> Vcalendar -> IO ()
+writeVCalendar hdl cal = hPutStrLn hdl (show cal) 
 
 
-getICS :: FilePath -> IO String 
-getICS = 
+doPrintTest :: IO () 
+doPrint = do h <- makeICS "test"
+             writeVEvent h testCal
+             hClose h
+             return ()
+
+-- getICS :: String -> Either IO ()
+-- getICS name = writeFile name 
 
